ccb0e38f322a440836c48d939691b4903251e5fc
Elliot Burin <eburin2@gmail.com>
2019-04-04 14:07:32 -0700

    add relations between products and users

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -25,10 +28,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({force: true})
   .then(result => {
-    console.log(result);
+    // console.log(result);
     app.listen(3000);
   })
   .catch(err => {

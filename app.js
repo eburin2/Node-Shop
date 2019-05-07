449e8b3f7d354bbcf7efde4ff3aec09e181404e0
Elliot Burin <eburin2@gmail.com>
2019-05-07 10:31:27 -0700

    add sessions to mongoDB database

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Elliot:@flipper1984@cluster0-v2txv.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collections: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,7 +28,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({secret: 'my secret', resave: false, saveUninitialized: false})
+  session({secret: 'my secret', resave: false, saveUninitialized: false, store: store})
 );
 
 app.use((req, res, next) => {
@@ -40,7 +47,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://Elliot:@flipper1984@cluster0-v2txv.mongodb.net/shop?retryWrites=true',
+  .connect(
+    MONGODB_URI,
   { useNewUrlParser: true }
   )
   .then(result => {
